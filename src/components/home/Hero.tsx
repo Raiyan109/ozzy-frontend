984f33b8fd40340a11f89ee2fe06a846dd16d45c
Raiyan109 <kraiyan109@gmail.com>
2025-01-13 14:57:05 +0600

    hero desktop done

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -9,9 +9,14 @@ const Hero = () => {
             // style={{
             //     backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.8) 100%), url(${heroImage.src})`,
             // }}
-            className=" bg-cover xl:bg-cover bg-no-repeat bg-center  h-[50vh] md:h-[70vh] lg:h-[calc(100vh-2px)] xl:h-[calc(100vh-64px)] w-full mt-16 md:mt-16 lg:mt-16 xl:mt-16"
+            className=" bg-cover xl:bg-cover bg-no-repeat bg-center  h-[50vh] md:h-[70vh] lg:h-[calc(100vh-2px)] xl:h-[calc(100vh-64px)] w-full mt-16 md:mt-16 lg:mt-16 xl:mt-16 relative"
         >
-            <p className="text-white text-5xl">dfdf</p>
+            <div className="absolute top-[30%] left-[11%] space-y-5">
+                <p className="text-white/70 text-sm">Find Your Energy</p>
+                <h1 className="text-white text-6xl max-w-lg">Make Your Body Healthy & Fit</h1>
+                <p className="text-white/70 max-w-xl text-sm">Gymhen an unknown printer took a galley of type and scrambled.It has survived nknown printercenturies.</p>
+                <button className="btn">BOOK APPOINTMENT </button>
+            </div>
         </div>
     )
 }
