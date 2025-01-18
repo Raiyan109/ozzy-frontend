48ad91c7f5a4bf8e18766081a48a85d2f457d338
Raiyan109 <kraiyan109@gmail.com>
2025-01-18 11:59:50 +0600

    sign up page done

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,5 +1,9 @@
 import signupPage from '@/assets/signup.png'
 import Image from 'next/image'
+import Link from 'next/link';
+import { CiMail, CiLock } from "react-icons/ci";
+import { FaPhone } from "react-icons/fa6";
+import { TbGenderAndrogyne } from "react-icons/tb";
 
 const Signup = () => {
     return (
@@ -18,44 +22,45 @@ const Signup = () => {
                     </div>
 
                     <div className="flex items-center md:p-8 p-6 bg-[#345C8C] h-full lg:w-11/12 lg:ml-auto">
-                        <form className="max-w-lg w-full mx-auto">
-                            <h1 className="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
-                            <p className="text-sm font-normal text-gray-600 mb-7">Welcome Back</p>
-                            <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                        <form className="max-w-lg w-full mx-auto bg-[#FAFAFA66] p-7 rounded-xl">
+                            <h1 className="text-white font-bold text-2xl mb-1 text-center py-5">Sign up</h1>
+                            <div className="flex items-center border border-[#345C8C] py-2 px-3 rounded-full bg-white mb-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#345C8C]" viewBox="0 0 20 20"
                                     fill="currentColor">
                                     <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
                                         clipRule="evenodd" />
                                 </svg>
-                                <input className="pl-2 outline-none border-none bg-[#345C8C]" type="text" name="" id="" placeholder="Full name" />
+                                <input className="pl-5 outline-none border-none text-[#345C8C] placeholder:text-[#345C8C]" type="text" name="" id="" placeholder="Name" />
                             </div>
-                            <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                        d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
-                                </svg>
-                                <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Username" />
+                            <div className="flex items-center border border-[#345C8C] py-2 px-3 rounded-full bg-white mb-4">
+                                <CiMail className="h-5 w-5 text-[#345C8C] font-bold" />
+                                <input className="pl-5 outline-none border-none text-[#345C8C] placeholder:text-[#345C8C]" type="text" name="" id="" placeholder="Email" />
                             </div>
-                            <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                        d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                                </svg>
-                                <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
+                            <div className="flex items-center border border-[#345C8C] py-2 px-3 rounded-full bg-white mb-4">
+                                <TbGenderAndrogyne className="h-5 w-5 text-[#345C8C] font-bold" />
+                                <select className="pl-5 outline-none border-none text-[#345C8C] placeholder:text-[#345C8C] bg-transparent w-full">
+                                    <option value="" disabled selected>
+                                        Gender
+                                    </option>
+                                    <option value="option1">Male</option>
+                                    <option value="option2">Female</option>
+                                </select>
                             </div>
-                            <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path fillRule="evenodd"
-                                        d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                        clipRule="evenodd" />
-                                </svg>
-                                <input className="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" />
+
+                            <div className="flex items-center border border-[#345C8C] py-2 px-3 rounded-full bg-white mb-4">
+                                <FaPhone className="h-4 w-4 text-[#345C8C]" />
+                                <input className="pl-5 outline-none border-none text-[#345C8C] placeholder:text-[#345C8C]" type="text" name="" id="" placeholder="Phone Number" />
                             </div>
-                            <button type="submit" className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
-                            <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
+                            <div className="flex items-center border border-[#345C8C] py-2 px-3 rounded-full bg-white mb-4">
+                                <CiLock className="h-5 w-5 text-[#345C8C] font-bold" />
+                                <input className="pl-5 outline-none border-none text-[#345C8C] placeholder:text-[#345C8C]" type="text" name="" id="" placeholder="Password" />
+                            </div>
+
+                            <button type="submit" className="block w-full bg-[#345C8C] mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Register</button>
+                            <div className='text-center'>
+                                <span className="text-sm ml-2 cursor-pointer  text-white">Already  have an account ?  </span>
+                                <Link href='/signin' className='text-[#345C8C]'>Sign In</Link>
+                            </div>
                             {/* <div className="mb-12">
                                 <h3 className="text-2xl font-bold text-yellow-400">Create an account</h3>
                             </div>
