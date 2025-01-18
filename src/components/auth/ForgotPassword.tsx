28617c8dbaa92f8d72b4f3850250abcb338be621
Raiyan109 <kraiyan109@gmail.com>
2025-01-18 12:27:32 +0600

    forgot password done

--- src/components/auth/ForgotPassword.tsx
+++ src/components/auth/ForgotPassword.tsx
@@ -2,6 +2,7 @@ import forgotImg from '@/assets/forgot.png'
 import Image from 'next/image'
 import Link from 'next/link';
 import { CiMail, CiLock } from "react-icons/ci";
+import { FaArrowLeft } from "react-icons/fa";
 
 const ForgotPassword = () => {
     return (
@@ -12,7 +13,7 @@ const ForgotPassword = () => {
                         {/* <img src="https://readymadeui.com/signin-image.webp" className="lg:max-w-[85%] w-full h-full aspect-square object-contain block mx-auto" alt="login-image" /> */}
                         <Image
                             src={forgotImg}
-                            alt='Sign up page'
+                            alt='Forgot password image'
                             height={800}
                             width={800}
                             className="lg:max-w-[85%] w-full h-full aspect-square object-contain block mx-auto"
@@ -21,31 +22,25 @@ const ForgotPassword = () => {
 
                     <div className="flex items-center md:p-8 p-6 bg-[#345C8C] h-full lg:w-11/12 lg:ml-auto">
                         <form className="max-w-lg w-full mx-auto bg-[#FAFAFA66] p-7 rounded-xl">
-                            <h1 className="text-white font-bold text-2xl mb-1 text-center py-5">Sign in</h1>
+                            <div className='space-y-6 mb-6'>
+                                <div className='flex items-center gap-5 justify-center'>
+                                    <FaArrowLeft className='text-white h-5 w-5' />
+                                    <h1 className="text-white font-bold text-2xl text-center">Forgot Password</h1>
+                                </div>
+                                <p className='text-white/75 text-center text-sm'>Please enter your email address to reset
+                                    your password.</p>
+                            </div>
 
-                            <div className="flex items-center border border-[#345C8C] py-2 px-3 rounded-full bg-white mb-4">
+                            <div className="flex items-center border border-[#345C8C] py-2 px-3 rounded-full bg-white mb-6">
                                 <CiMail className="h-5 w-5 text-[#345C8C] font-bold" />
                                 <input className="pl-5 outline-none border-none text-[#345C8C] placeholder:text-[#345C8C]" type="text" name="" id="" placeholder="Email" />
                             </div>
-                            <div className="flex items-center border border-[#345C8C] py-2 px-3 rounded-full bg-white mb-4">
-                                <CiLock className="h-5 w-5 text-[#345C8C] font-bold" />
-                                <input className="pl-5 outline-none border-none text-[#345C8C] placeholder:text-[#345C8C]" type="text" name="" id="" placeholder="Password" />
-                            </div>
-                            <div className='flex justify-between items-center'>
-                                <div className="flex items-center ">
-                                    <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 rounded" />
-                                    <label htmlFor="remember-me" className="text-white ml-3 block text-sm">
-                                        Remember me
-                                    </label>
-                                </div>
-                                <Link href='/forgot-password' className="text-white ml-3 block text-sm hover:text-[#345C8C]">Forgot password?</Link>
-                            </div>
-                            <button type="submit" className="block w-full bg-[#345C8C] mt-4 py-2 rounded-full text-white font-semibold mb-2">Sign in</button>
-                            <div className='text-center'>
-                                <span className="text-sm ml-2 cursor-pointer  text-white">Don't have an account ?  </span>
-                                <Link href='/signup' className='text-[#345C8C]'>Sign up</Link>
-                            </div>
 
+                            <button type="submit" className="block w-full bg-[#345C8C] mt-4 py-2 rounded-full text-white font-semibold mb-2">Send OTP</button>
+                            <div className='text-center'>
+                                <span className="text-sm ml-2 cursor-pointer  text-white">Already  have an account ?  </span>
+                                <Link href='/signin' className='text-[#345C8C]'>Sign In</Link>
+                            </div>
                         </form>
                     </div>
                 </div>
