9b9760b9af7523e3f073d9843cb2eea36473de06
Raiyan109 <kraiyan109@gmail.com>
2025-01-19 11:44:56 +0600

    now removing next video

--- src/app/(WithCommonLayout)/(home)/make-plan/page.tsx
+++ src/app/(WithCommonLayout)/(home)/make-plan/page.tsx
@@ -1,10 +1,13 @@
 import MakePlan from "@/components/makeWorkoutPlan/MakePlan"
+import Video from 'next-video'
+import Snow from '../../../../../videos/snow.mp4.mp4'
 
 
 const MakePlanPage = () => {
     return (
         <div>
-            <MakePlan />
+            <Video src={Snow} />
+            {/* <MakePlan /> */}
         </div>
     )
 }
