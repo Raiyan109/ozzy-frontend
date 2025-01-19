8f0a61f41706a34f946ebb6917e24c1cbe9700c6
Raiyan109 <kraiyan109@gmail.com>
2025-01-19 10:33:33 +0600

    workouit details page

--- src/app/(WithCommonLayout)/(home)/workout/[workoutId]/page.tsx
+++ src/app/(WithCommonLayout)/(home)/workout/[workoutId]/page.tsx
@@ -1,4 +1,64 @@
 import WorkoutDetails from "@/components/workoutComponents/WorkoutDetails";
+import WorkoutImg from "@/assets/workout-img.png"
+
+const workouts = [
+    {
+        id: 1,
+        name: 'Monday-Chest day',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita suscipit error quas porro qui. Non',
+        duration: '10 minutes',
+        image: WorkoutImg
+    },
+    {
+        id: 2,
+        name: 'Monday-Chest day',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita suscipit error quas porro qui. Non',
+        duration: '10 minutes',
+        image: WorkoutImg
+    },
+    {
+        id: 3,
+        name: 'Monday-Chest day',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita suscipit error quas porro qui. Non',
+        duration: '10 minutes',
+        image: WorkoutImg
+    },
+    {
+        id: 4,
+        name: 'Monday-Chest day',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita suscipit error quas porro qui. Non',
+        duration: '10 minutes',
+        image: WorkoutImg
+    },
+    {
+        id: 5,
+        name: 'Monday-Chest day',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita suscipit error quas porro qui. Non',
+        duration: '10 minutes',
+        image: WorkoutImg
+    },
+    {
+        id: 6,
+        name: 'Monday-Chest day',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita suscipit error quas porro qui. Non',
+        duration: '10 minutes',
+        image: WorkoutImg
+    },
+    {
+        id: 7,
+        name: 'Monday-Chest day',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita suscipit error quas porro qui. Non',
+        duration: '10 minutes',
+        image: WorkoutImg
+    },
+    {
+        id: 8,
+        name: 'Monday-Chest day',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita suscipit error quas porro qui. Non',
+        duration: '10 minutes',
+        image: WorkoutImg
+    },
+]
 
 interface WorkoutId {
     params: {
@@ -8,11 +68,23 @@ interface WorkoutId {
 
 const WorkoutDetailsPage = async ({ params }: { params: { workoutId: string } }) => {
     const { workoutId } = await params;
-    console.log(workoutId, 'workout details id');
+
+    // Find the workout that matches the workoutId
+    const workout = workouts.find(workout => workout.id === parseInt(workoutId));
+    console.log(workout, 'from WorkoutDetailsPage');
+
+
+    if (!workout) {
+        return (
+            <div>
+                <h1>Workout not found</h1>
+            </div>
+        );
+    }
 
     return (
         <div>
-            <WorkoutDetails workoutId={workoutId} />
+            <WorkoutDetails workout={workout} />
         </div>
     )
 }
--- src/components/workoutComponents/WorkoutDetails.tsx
+++ src/components/workoutComponents/WorkoutDetails.tsx
@@ -1,22 +1,35 @@
-'use client'
+// 'use client'
 import { useAppSelector } from "@/redux/hooks";
-import Image from "next/image"
+import Image, { StaticImageData } from "next/image"
 
+type Props = {
+    name: string;
+    description: string;
+    duration: string;
+    image: StaticImageData
+}
 
-const WorkoutDetails = ({ workoutId }: { workoutId: string }) => {
-    const state = useAppSelector(state => state.test)
-    console.log(state);
+const WorkoutDetails = ({ workout }: { workout: Props }) => {
+    console.log(workout.image.src, 'WorkoutDetails');
+
     return (
-        <div>
-            <div>
-                {/* <Image
-                                    src={logo}
-                                    alt='Ozzy Logo'
-                                    height={80}
-                                    width={80}
-                                /> */}
+        <div className=" mt-[65px] mx-auto max-w-[1500px] px-6 sm:px-6 lg:px-8 py-32 flex">
+            <div className="flex-1 space-y-4">
+                <Image
+                    src={workout.image}
+                    alt='Ozzy Logo'
+                    height={300}
+                    width={300}
+                    className="h-[300px] w-[500px] object-cover rounded-xl"
+                />
+                <div className="flex items-center gap-64">
+                    <h3 className="text-[#4E4E4E]">{workout.name}</h3>
+                    <button className="btn min-w-[60px] px-2 ">Continue</button>
+                </div>
+                <p className="text-[#4E4E4E]/80 text-sm max-w-sm">{workout.description}</p>
+                <p className="text-[#4E4E4E]/80 text-sm max-w-sm">{workout.duration}</p>
             </div>
-            <div></div>
+            <div className="flex-1"></div>
         </div>
     )
 }
