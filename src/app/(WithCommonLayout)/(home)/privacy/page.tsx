0e3d676dceca12415ba08b1aafe98434a4e20c98
Raiyan109 <kraiyan109@gmail.com>
2025-01-20 12:34:20 +0600

    privacy page

---
+++ src/app/(WithCommonLayout)/(home)/privacy/page.tsx
@@ -0,0 +1,13 @@
+import Privacy from "@/components/otherPages/Privacy"
+
+
+
+const PrivacyPage = () => {
+    return (
+        <div>
+            <Privacy />
+        </div>
+    )
+}
+
+export default PrivacyPage
